66febb22c4fda5f8fc3bf1bb593639407ccf92e2
edelmanjm <edelmanjm@users.noreply.github.com>
2018-04-14 14:03:04 -0700

    Add acceleration interpolation

--- src/main/java/org/team1540/robot2018/RobotRecording.java
+++ src/main/java/org/team1540/robot2018/RobotRecording.java
@@ -6,7 +6,10 @@ import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.Trajectory;
 import jaci.pathfinder.Trajectory.Segment;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map.Entry;
+import org.apache.commons.math3.analysis.interpolation.SplineInterpolator;
 import org.team1540.base.util.SimpleLoopCommand;
 import org.team1540.base.wrappers.ChickenTalon;
 import org.team1540.robot2018.RecordProfile.Storage;
@@ -18,6 +21,9 @@ public class RobotRecording extends IterativeRobot {
   private RecordProfile drivetrainRecording = new RecordProfile(drivetrain.driveLeftMotorA,
       drivetrain.driveRightMotorA);
   public static String path = "/home/lvuser/generated-profiles/";
+
+  public int degreesToFit = 4;
+  public int pointsToUse = 2;
 
   public RobotRecording() {
     LiveWindow.disableAllTelemetry();
@@ -42,10 +48,44 @@ public class RobotRecording extends IterativeRobot {
     String folderPath = path + System.currentTimeMillis() + "/";
     for (Entry<ChickenTalon, Storage> entry : drivetrainRecording.getMotors().entrySet()) {
       if (entry.getValue().segments.size() > 1) {
+        List<Double> times = new ArrayList<>(2 * pointsToUse + 1);
+        List<Double> velocities = new ArrayList<>(2 * pointsToUse + 1);
+
+        for (int i = 0; i < entry.getValue().segments.size(); i++) {
+          // Acceleration and jerk interpolation
+          // TODO use Apache math fun? Way more flexibility, potentially better fits
+
+          times.clear();
+          velocities.clear();
+          double timeTotal = 0;
+          double targetTime = 0;
+          for (int j = -pointsToUse; j <= pointsToUse; j++) {
+            int newIndex = i+j;
+            if (!(newIndex < 0 || newIndex > entry.getValue().segments.size() - 1)) {
+              Segment segment = entry.getValue().segments.get(newIndex);
+              timeTotal += segment.dt;
+              if (j == 0) {
+                targetTime = timeTotal;
+              }
+              times.add(timeTotal);
+              velocities.add(segment.velocity);
+            }
+          }
+
+          // Set the acceleration to the value of the derivative of the interpolated function
+          entry.getValue().segments.get(i).acceleration =
+              new SplineInterpolator().interpolate(times.stream().mapToDouble
+              (Double::doubleValue).toArray(), velocities.stream().mapToDouble
+              (Double::doubleValue).toArray()).derivative().value(targetTime);
+
+        }
+
+        // Writing out
         Trajectory thisTrajectory = new Trajectory(entry.getValue().segments.toArray(new
             Segment[entry.getValue().segments.size()]));
         // RecordProfile.timeDialateTrajectory(thisTrajectory, timeDialation);
         File file = new File( folderPath + entry.getKey().getDeviceID() + ".csv");
+        //noinspection ResultOfMethodCallIgnored
         file.getParentFile().mkdirs();
         Pathfinder.writeToCSV(file, thisTrajectory);
       }
