5339260886ddc711b816ca00a07ea0f58c39cade
wangl5 <wangl5@users.noreply.github.com>
2018-04-18 15:10:21 -0700

    Added save function

--- src/main/java/org/team1540/robot2018/RobotRecording.java
+++ src/main/java/org/team1540/robot2018/RobotRecording.java
@@ -1,15 +1,17 @@
 package org.team1540.robot2018;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.Trajectory;
 import jaci.pathfinder.Trajectory.Segment;
 import java.io.File;
-import java.util.Map.Entry;
+import org.team1540.base.adjustables.Tunable;
+import org.team1540.base.util.SimpleCommand;
 import org.team1540.base.util.SimpleLoopCommand;
-import org.team1540.base.wrappers.ChickenTalon;
-import org.team1540.robot2018.RecordProfile.Storage;
+import org.team1540.robot2018.motion.FollowProfile;
 import org.team1540.robot2018.subsystems.DriveTrain;
 
 public class RobotRecording extends IterativeRobot {
@@ -19,8 +21,19 @@ public class RobotRecording extends IterativeRobot {
       drivetrain.driveRightMotorA);
   public static String path = "/home/lvuser/generated-profiles/";
 
+  private Trajectory leftTrajectory;
+  private Trajectory rightTrajectory;
+
+  @Tunable("Time dialation")
+  public double timeDialation = 1;
+
   public RobotRecording() {
     LiveWindow.disableAllTelemetry();
+  }
+
+  @Override
+  public void robotInit() {
+    SmartDashboard.putData(new SimpleCommand("Save Recorded Profile", this::saveTrajectories));
   }
 
   @Override
@@ -39,22 +52,38 @@ public class RobotRecording extends IterativeRobot {
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
-    String folderPath = path + System.currentTimeMillis() + "/";
-    for (Entry<ChickenTalon, Storage> entry : drivetrainRecording.getMotors().entrySet()) {
-      processSegments(entry.getValue().segments.toArray(new Segment[entry.getValue().segments
-          .size()]), folderPath + entry.getKey().getDeviceID());
+
+    leftTrajectory = new Trajectory(drivetrainRecording.getMotors().get(drivetrain.driveLeftMotorA).segments.toArray(new Segment[0]));
+    rightTrajectory = new Trajectory(drivetrainRecording.getMotors().get(drivetrain.driveRightMotorA).segments.toArray(new Segment[0]));
+
+    RecordProfile.timeDialateTrajectory(leftTrajectory, timeDialation);
+    RecordProfile.timeDialateTrajectory(rightTrajectory, timeDialation);
+
+    // String folderPath = path + System.currentTimeMillis() + "/";
+    // for (Entry<ChickenTalon, Storage> entry : drivetrainRecording.getMotors().entrySet()) {
+    //   processSegments(entry.getValue().segments.toArray(new Segment[entry.getValue().segments
+    //       .size()]), folderPath + entry.getKey().getDeviceID());
+    // }
+  }
+
+  public void saveTrajectories() {
+    File leftFile = new File(path + System.currentTimeMillis() + "/left.csv");
+    File rightFile = new File(path + System.currentTimeMillis() + "/right.csv");
+    //noinspection ResultOfMethodCallIgnored
+    leftFile.getParentFile().mkdirs();
+    Pathfinder.writeToCSV(leftFile, leftTrajectory);
+    Pathfinder.writeToCSV(rightFile, rightTrajectory);
+  }
+
+  @Override
+  public void autonomousInit() {
+    if (leftTrajectory != null && rightTrajectory != null) {
+      new FollowProfile(leftTrajectory, rightTrajectory);
     }
   }
 
-  public void processSegments(Segment[] segments, String path){
-    if (segments.length > 1) {
-      // Writing out
-      Trajectory thisTrajectory = new Trajectory(segments);
-      // RecordProfile.timeDialateTrajectory(thisTrajectory, timeDialation);
-      File file = new File(path + ".csv");
-      //noinspection ResultOfMethodCallIgnored
-      file.getParentFile().mkdirs();
-      Pathfinder.writeToCSV(file, thisTrajectory);
-    }
+  @Override
+  public void robotPeriodic() {
+    Scheduler.getInstance().run();
   }
 }
